67ff2823de635e7f14ffb55f27c591bbf2c64c1b
ddophi98 <lightcloud98@gmail.com>
2022-11-20 18:28:41 +0900

    행 구현 및 double extension 생성

--- Crypto/Crypto/Core/Components/CircleButton/CircleButtonView.swift
+++ Crypto/Crypto/Core/Components/CircleButton/CircleButtonView.swift
@@ -37,7 +37,7 @@ struct CircleButtonView_Previews: PreviewProvider {
                 .previewLayout(.sizeThatFits)
             CircleButtonView(iconName: "plus")
                 .previewLayout(.sizeThatFits)
-                .colorScheme(.dark)
+                .preferredColorScheme(.dark)
         }
     }
 }
