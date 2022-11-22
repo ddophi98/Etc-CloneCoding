0c93f3755db0046fa81921497baed6b2fbca79e5
ddophi98 <lightcloud98@gmail.com>
2022-11-22 15:36:31 +0900

    코인 이미지 다운로드

---
+++ Crypto/Crypto/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,38 @@
+//
+//  CoinImageView.swift
+//  Crypto
+//
+//  Created by 김동락 on 2022/11/22.
+//
+
+import SwiftUI
+
+struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundColor(.theme.secondaryText)
+            }
+        }
+    }
+}
+
+struct CoinImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinImageView(coin: dev.coin)
+    }
+}
---
+++ Crypto/Crypto/Core/Components/CoinImage/CoinImageViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  CoinImageViewModel.swift
+//  Crypto
+//
+//  Created by 김동락 on 2022/11/22.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    @Published var isLoading: Bool = false
+    
+    private let coin: CoinModel
+    private let dataService: CoinImageService
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        self.dataService = CoinImageService(coin: coin)
+        self.isLoading = true
+        addSubscribers()
+    }
+    
+    private func addSubscribers() {
+        dataService.$image
+            .sink { [weak self] _ in
+                self?.isLoading = false
+            } receiveValue: { [weak self] returnedImage in
+                self?.image = returnedImage
+            }
+            .store(in: &cancellables)
+
+    }
+}
