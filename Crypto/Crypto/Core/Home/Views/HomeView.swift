ba7f6c3ff6ee832d6c7fe4907cbe6edd4ded867e
ddophi98 <lightcloud98@gmail.com>
2022-12-29 13:36:13 +0900

    전체적으로 최적화하기

--- Crypto/Crypto/Core/Home/Views/HomeView.swift
+++ Crypto/Crypto/Core/Home/Views/HomeView.swift
@@ -35,8 +35,14 @@ struct HomeView: View {
                         .transition(.move(edge: .leading))
                 }
                 if showPortfolio {
-                    portfolioCoinsList
-                        .transition(.move(edge: .trailing))
+                    ZStack(alignment: .top) {
+                        if vm.portfolioCoins.isEmpty && vm.searchText.isEmpty {
+                            portfolioEmptyText
+                        } else {
+                            portfolioCoinsList
+                        }
+                    }
+                    .transition(.move(edge: .trailing))
                 }
                 
                 
@@ -123,6 +129,15 @@ extension HomeView {
             }
         }
         .listStyle(PlainListStyle())
+    }
+    
+    private var portfolioEmptyText: some View {
+        Text("You haven't added any coins to your portfolio yet. Click the + button to get started!! 😎")
+            .font(.callout)
+            .foregroundColor(.theme.accent)
+            .fontWeight(.medium)
+            .multilineTextAlignment(.center)
+            .padding(50)
     }
     
     private func segue(coin :CoinModel) {
